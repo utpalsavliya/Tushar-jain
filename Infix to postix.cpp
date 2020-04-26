8f460990cdc8ec867ed0e8ae9541128edd485710
Utpal Savaliya <utpalsavliya@gmail.com>
2020-04-26 21:33:46 +0530

    Comment on mistakes

--- Infix to postix.cpp
+++ Infix to postix.cpp
@@ -6,8 +6,8 @@ using namespace std;
 
 int GetOperatorWeight(char op)
 {
-  int weight = -1;
-  switch(op)
+  int weight = -1;      //Brackets '(', ')' will be assigned lowest weight, either increse this
+  switch(op)            //Or add case for brackets too
   {
   case '+':
   case '-':
@@ -34,7 +34,7 @@ int higherprecedence(char op1 , char op2)
 
 string itp(string exp)
 {
-  int i,j;
+  int i,j;                    //j unused, remove j
   stack<char> S;
   string res;
   res.clear();
@@ -43,26 +43,27 @@ string itp(string exp)
     char ch=exp[i];
     if(isdigit(ch)){
       res+=ch;
-    }
+    }                   //Add check for other inputs
     else{
       if(ch=='('){
         S.push(ch);
       }
       else if(ch==')')
-      {
-        while(S.empty()!=0 && S.top()!='(')
+      {           //S.empty() true when stack empty. S.empty()!=0 false when stack not empty
+        while(S.empty()!=0 && S.top()!='(') //Change to S.empty()!= 0 or !S.empty()
         {
           res+=S.top();
           S.pop();
         }
         S.pop();
       }
-      else if(higherprecedence(S.top(),ch)==0){
-      S.push(ch);
+      else if(higherprecedence(S.top(),ch)==0){   //S.top() will error if empty stack, add checking condition
+        S.push(ch);
       }
       else if(higherprecedence(S.top(),ch)==1)
-      {
-        while(S.top()=='/'||S.top()=='*'||S.top()=='-'||S.top()=='+'){
+      {       //Only pop till weight of S.top() >= weight of ch
+              //Eg.-stack = ['-', '+', '*', ('/')], top = '/', ch = '/'. Then pop till '*'
+        while(S.top()=='/'||S.top()=='*'||S.top()=='-'||S.top()=='+'){  //Add appropriate logic
           res+=S.top();
           S.pop();
         }
@@ -70,16 +71,16 @@ string itp(string exp)
       }
     }
   }
-  while(S.empty()!=0)
-  {
+  while(S.empty()!=0) //S.empty() true when stack empty. S.empty()!=0 false when stack not empty
+  {                   //Change to S.empty()!= 0 or !S.empty()
     res+=S.top();
     S.pop();
   }
   return res;
 }
-int main(){
+int main(){       //Provide some info about program to user
   string exp;
-  cin>>exp;
+  cin>>exp;       //Add input prompt message
   string postfix = itp(exp);
   cout<<"Output = "<<postfix<<"\n";
 }
